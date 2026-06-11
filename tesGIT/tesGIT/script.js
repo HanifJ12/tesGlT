cd4fd31c09b0dc35366712735e6856325a650339
HanifJ12 <111216167+HanifJ12@users.noreply.github.com>
2026-06-11 16:32:17 +0700

    Delete tesGIT directory

--- tesGIT/tesGIT/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const tombol = document.getElementById('tombolSapa');
-    
-    if (tombol) {
-        tombol.addEventListener('click', function() {
-            alert('Mantap! Website statis dari folder tesGIT sudah berfungsi dengan baik. Waktunya deploy ke GitHub!');
-        });
-    }
-});
